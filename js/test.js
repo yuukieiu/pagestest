0795e40ef636b1c2a900621a8b5fb0f44483cf69
yuuki <yuukieiu@gmail.com>
2022-04-17 00:12:54 +0900

    ログイン中のユーザー表示

--- js/test.js
+++ js/test.js
@@ -4,11 +4,13 @@ function onSignIn(googleUser) {
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    document.getElementById("UserMailAddress").textContent = profile.getEmail();
   }
 
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
+      document.getElementById("UserMailAddress").textContent = "";
     });
   }
